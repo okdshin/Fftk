2096671f0383c88bb87b7b68443289298c0121d3
okada <kokuzen@gmail.com>
2013-10-22 00:40:39 +0900

    implement DiscreteFourierTransform

--- Test/DiscreteFourierTransform.cpp
+++ Test/DiscreteFourierTransform.cpp
@@ -7,13 +7,16 @@ using namespace fftk;
 
 int main(int argc, char* argv[])
 {
+	unsigned int signal_len = 8;
 	std::vector<std::complex<double>> source_signal;
-	for(unsigned int i = 0; i < 16; ++i){
-		source_signal.push_back(std::complex<double>(i, 0));
+	for(double i = 0; i < 2*M_PI; i+=(2*M_PI/static_cast<double>(signal_len))){
+		source_signal.push_back(std::complex<double>(cos(i)+cos(i*10), 0));
 	}
-	Output(std::cout, source_signal);
-	auto dest_signal = Dft(source_signal);
-	Output(std::cout, dest_signal);
+	DiscreteFourierTransform dft;
+	auto dest_signal = dft.Transform(source_signal);
+	Output(std::cout << "transform: ", dest_signal);
+	Output(std::cout << "inverse: ", dft.InverseTransform(dest_signal));
+	Output(std::cout << "source: ", source_signal);
 
 	FFTK_CHECK_EQUAL(dest_signal, dest_signal);
 
--- Test/DiscreteFourierTransform.h
+++ Test/DiscreteFourierTransform.h
@@ -23,4 +23,35 @@ auto Dft(const std::vector<std::complex<double>>& signal) -> std::vector<std::co
 	return res;
 }
 
+class DiscreteFourierTransform{
+public:
+	auto Transform(const std::vector<std::complex<double>>& signal) -> std::vector<std::complex<double>> {
+		return DoTransform(signal, false);
+	}
+
+	auto InverseTransform(const std::vector<std::complex<double>>& signal) -> std::vector<std::complex<double>> {
+		auto res = DoTransform(signal, true);
+		for(auto& e : res){
+			e /= static_cast<double>(signal.size());
+		}
+		return res;
+	}
+
+private:
+	auto DoTransform(const std::vector<std::complex<double>>& signal, bool is_inv) -> std::vector<std::complex<double>> {
+		const auto N = signal.size();
+		std::vector<std::complex<double>> res;
+		res.reserve(N);
+		for(unsigned int i = 0; i < N; ++i){
+			std::complex<double> sum(0,0);
+			for(unsigned int k = 0; k < N; ++k){
+				double t = is_inv ? 2*M_PI*i*k/N : (-2*M_PI*i*k)/N;	
+				sum += signal[k]*exp(std::complex<double>(0, t));
+			}
+			res.push_back(sum);
+		}
+		return res;
+	}
+};
+
 }
